a5273e85532b78de44706725c5e10ab7f9c6e64d
alsaadahamed4 <alsaadahamed4@gmail.com>
2020-05-15 20:51:11 +0530

    destructuring of objects by  naming and default values.

---
+++ src/playGround/destructuring.js
@@ -0,0 +1,47 @@
+console.log('destructuring');
+
+const person = {
+    name: 'Alsaad',
+    age: 23,
+    location: {
+        city: 'majoor',
+        temp: 36
+    }
+};
+
+console.log(`Hello ${person.name} from ${person.location.city}`);
+
+//destructuring
+
+const { name, age } = person;
+const { city, temp } = person.location;
+
+console.log(`Hello am ${name} aged about ${age} years and currently reside in ${city} with temparature about ${temp} degree`);
+
+
+//destructuring with naming and default value
+
+const { name: pName = 'saad', age: pAge } = person;
+const { city: pCity, temp: pTemp } = person.location;
+
+console.log(`Hello am ${pName} aged about ${pAge} years and currently reside in ${pCity} with temparature about ${pTemp} degree`);
+
+
+const book = {
+    title: 'Two States',
+    author: 'Ahamed',
+    publisher: {
+        name: 'Alsaad'
+    }
+}
+
+console.log(`The book called ${book.title} was written by ${book.author} and published by ${book.publisher.name}`);
+
+//destructuring and default value
+
+const { title, author } = book;
+const { name: pubName = 'self' } = book.publisher;
+
+console.log(`The book called ${title} was written by ${author} and published by ${pubName}`);
+
+
