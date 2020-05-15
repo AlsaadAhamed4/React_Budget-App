16f511aee4f118db2d9b620b86c9b6b9f07f92ac
alsaadahamed4 <alsaadahamed4@gmail.com>
2020-05-15 19:44:26 +0530

    Simple Redux actions to change the state by dispatching to store

--- src/playGround/redux-101.js
+++ src/playGround/redux-101.js
@@ -1,8 +1,45 @@
 import { createStore } from 'redux'
 
-const store = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 },action) => {  //in store the 2nd args is action 
+    switch(action.type){
+        case 'INCREMENT':
+            return{
+                count : state.count + 1
+            }
+        case 'DECREMENT':
+            return{
+                count : state.count - 1
+            }
+        case 'RESET':
+            return{
+                count : 0
+            }
+        default:
+            return state;    
+    }
 })
 
 
 console.log(store.getState());
+
+//actions are the object that are sent to the store 
+//only through actions we can change the state in redux
+//actions are always dispatched to the store
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+store.dispatch({
+    type:'RESET'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+console.log(store.getState()); //here we have changed the state 
