4472924c497efe4aa02230b652fb3f3852b71d30
alsaadahamed4 <alsaadahamed4@gmail.com>
2020-05-16 17:31:12 +0530

    created expense redux with initial state and created a store by passing combined reducer one for expenses and filters.

--- src/playGround/redux-expensify.js
+++ src/playGround/redux-expensify.js
@@ -1,5 +1,63 @@
 import { createStore, combineReducers } from 'redux'
 
+//*********Actions*********** */
+//ADD_EXPENSE
+//REMOVE_EXPENSE
+//EDIT_EXPENSE
+//SET_TEXT_FILTER
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END_DATE
+//*********Actions*********** */
+
+//we have complex state to mentain we need two reducers
+// 1 ExpenseReducer
+// 2 filterReducer
+
+
+//ExpenseReducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        /*  case 'ADD_EXPENSE':
+             return{
+ 
+             }
+         case 'REMOVE_EXPENSE': */
+        default: return state;
+    }
+};
+
+//filterreducer
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default: return state;
+    }
+};
+
+//store creation and combined reducer
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters : filtersReducer
+    })
+);
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({
+    type: 'Hello'
+});
 
 const demoState = {
     expenses: [{
