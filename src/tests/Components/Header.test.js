ea6c7598310c43e9c10700157640c02f05fac0e8
alsaadahamed4 <alsaadahamed4@gmail.com>
2020-05-23 16:13:52 +0530

    added enzyme, adapter, raf library instead of react-renderer for testing component.

--- src/tests/Components/Header.test.js
+++ src/tests/Components/Header.test.js
@@ -1,14 +1,17 @@
 //virtually rendering the component
 //react -library to do it react-test-render
 
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+//import ReactShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react'
 import Header from '../../components/Header';
+import { shallow } from 'enzyme'
 
 
 test('should render header correctly', () => {
-    const renderer = new ReactShallowRenderer
-    renderer.render(<Header/>);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header/>);
+    expect(wrapper).toMatchSnapshot();
+    // const renderer = new ReactShallowRenderer
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
     //console.log(renderer.getRenderOutput());
 })
