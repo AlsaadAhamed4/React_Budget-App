35889b34cd739dab5f667d0e27f6ccef3db3e2f7
alsaadahamed4 <alsaadahamed4@gmail.com>
2020-05-26 22:28:40 +0530

    Test cases for ExpenseForm on user-Interaction.

--- src/tests/Components/ExpensesForm.test.js
+++ src/tests/Components/ExpensesForm.test.js
@@ -18,6 +18,56 @@ test('should render Expenses Form ', () => {
 });
 
 test('should render expense with data', () => {
-    const wrapper = shallow(<ExpenseForm editData={expenses[0]}/>);
+    const wrapper = shallow(<ExpenseForm editData={expenses[0]} />);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should render error for invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    //since we are have  preventOnDefault(e) so we need an args
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { }
+    });
+    expect(wrapper.state('errMsg').length).toBeGreaterThan(0); //to check If there's any error
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should set description on change', () => {
+    const value = 'new description';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(0).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('description')).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should set Note on change', () => {
+    const value = 'new Note';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('note')).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should set amount on valid input', () => {
+    const value = '2.50';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should set amount on in-valid input', () => {
+    const value = '2.501';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe('');
     expect(wrapper).toMatchSnapshot();
 })
