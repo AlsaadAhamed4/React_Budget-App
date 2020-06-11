32916804b0cecf70b1344dd00044eeef577fe4aa
alsaadahamed4 <alsaadahamed4@gmail.com>
2020-06-11 17:28:34 +0530

    Modified styles for expense-list related to link tag.

--- src/components/ExpensesListItem.js
+++ src/components/ExpensesListItem.js
@@ -6,13 +6,15 @@ import numeral from 'numeral';
 
 const ExpensesListItem = ({ description, amount, createdAt, id }) => (
     <div className='expenselist-item'>
-        <div className='expenselist-item__name'>
-            <Link className='expenselist-item--link' to={`/edit/${id}`}><h1>{description}</h1></Link>
-            <p>{moment(createdAt).format('MMMM Do, YYYY')}</p>
-        </div>
-        <div className='expenselist-item__amount'>
-            <p>{numeral(amount / 100).format('$0,0.00')}</p>
-        </div>
+        <Link className='expenselist-item--link' to={`/edit/${id}`}>
+            <div className='expenselist-item__name'>
+                <h1>{description}</h1>
+                <p>{moment(createdAt).format('MMMM Do, YYYY')}</p>
+            </div>
+            <div className='expenselist-item__amount'>
+                <p>{numeral(amount / 100).format('$0,0.00')}</p>
+            </div>
+        </Link>
     </div>
 );
 
